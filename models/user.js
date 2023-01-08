c43b1d9901b107046d57e56e50be713ee587b0aa
Kate3587 <108617406+Kate3587@users.noreply.github.com>
2023-01-08 20:51:10 +0100

    hw05-avatars

---
+++ models/user.js
@@ -0,0 +1,34 @@
+const { Schema, model } = require('mongoose')
+
+const userSchema = Schema({
+  password: {
+    type: String,
+    minlength: 6,
+    required: [true, 'Password is required'],
+  },
+  email: {
+    type: String,
+    match: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+    required: [true, 'Email is required'],
+    unique: true,
+  },
+  subscription: {
+    type: String,
+    enum: ["starter", "pro", "business"],
+    default: "starter"
+  },
+  token: {
+    type: String,
+    default: null,
+  },
+  avatarURL: {
+    type: String,
+    requered: true,
+  },
+},
+    { versionKey: false, timestamps: true }
+)
+
+const User = model('user', userSchema);
+
+module.exports = User;
